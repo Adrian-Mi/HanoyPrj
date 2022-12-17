75686f8dd015a6eb6a6963db3acbedc6305805cc
Adrian-Mi <120776299+Adrian-Mi@users.noreply.github.com>
2022-12-17 14:22:58 +0330

    Add files via upload

---
+++ HanoyPrj/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace HanoyPrj
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblBase = new System.Windows.Forms.Label();
+            this.LB = new System.Windows.Forms.Label();
+            this.LA = new System.Windows.Forms.Label();
+            this.LC = new System.Windows.Forms.Label();
+            this.L1 = new System.Windows.Forms.Label();
+            this.L2 = new System.Windows.Forms.Label();
+            this.L3 = new System.Windows.Forms.Label();
+            this.L4 = new System.Windows.Forms.Label();
+            this.L5 = new System.Windows.Forms.Label();
+            this.L6 = new System.Windows.Forms.Label();
+            this.L7 = new System.Windows.Forms.Label();
+            this.L8 = new System.Windows.Forms.Label();
+            this.L9 = new System.Windows.Forms.Label();
+            this.L10 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LblBase
+            // 
+            this.LblBase.BackColor = System.Drawing.Color.Pink;
+            this.LblBase.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.LblBase.Location = new System.Drawing.Point(0, 464);
+            this.LblBase.Name = "LblBase";
+            this.LblBase.Size = new System.Drawing.Size(800, 20);
+            this.LblBase.TabIndex = 0;
+            // 
+            // LB
+            // 
+            this.LB.BackColor = System.Drawing.Color.LightPink;
+            this.LB.Location = new System.Drawing.Point(400, 164);
+            this.LB.Name = "LB";
+            this.LB.Size = new System.Drawing.Size(20, 300);
+            this.LB.TabIndex = 1;
+            // 
+            // LA
+            // 
+            this.LA.BackColor = System.Drawing.Color.LightPink;
+            this.LA.Location = new System.Drawing.Point(150, 164);
+            this.LA.Name = "LA";
+            this.LA.Size = new System.Drawing.Size(20, 300);
+            this.LA.TabIndex = 2;
+            // 
+            // LC
+            // 
+            this.LC.BackColor = System.Drawing.Color.LightPink;
+            this.LC.Location = new System.Drawing.Point(650, 164);
+            this.LC.Name = "LC";
+            this.LC.Size = new System.Drawing.Size(20, 300);
+            this.LC.TabIndex = 1;
+            // 
+            // L1
+            // 
+            this.L1.BackColor = System.Drawing.Color.LightGreen;
+            this.L1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.L1.Location = new System.Drawing.Point(70, 444);
+            this.L1.Name = "L1";
+            this.L1.Size = new System.Drawing.Size(180, 20);
+            this.L1.TabIndex = 3;
+            this.L1.Tag = "1";
+            this.L1.Visible = false;
+            // 
+            // L2
+            // 
+            this.L2.BackColor = System.Drawing.Color.LightGreen;
+            this.L2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.L2.Location = new System.Drawing.Point(75, 424);
+            this.L2.Name = "L2";
+            this.L2.Size = new System.Drawing.Size(170, 20);
+            this.L2.TabIndex = 3;
+            this.L2.Tag = "2";
+            this.L2.Visible = false;
+            // 
+            // L3
+            // 
+            this.L3.BackColor = System.Drawing.Color.LightGreen;
+            this.L3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.L3.Location = new System.Drawing.Point(80, 404);
+            this.L3.Name = "L3";
+            this.L3.Size = new System.Drawing.Size(160, 20);
+            this.L3.TabIndex = 3;
+            this.L3.Tag = "3";
+            this.L3.Visible = false;
+            // 
+            // L4
+            // 
+            this.L4.BackColor = System.Drawing.Color.LightGreen;
+            this.L4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.L4.Location = new System.Drawing.Point(85, 384);
+            this.L4.Name = "L4";
+            this.L4.Size = new System.Drawing.Size(150, 20);
+            this.L4.TabIndex = 3;
+            this.L4.Visible = false;
+            // 
+            // L5
+            // 
+            this.L5.BackColor = System.Drawing.Color.LightGreen;
+            this.L5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.L5.Location = new System.Drawing.Point(90, 364);
+            this.L5.Name = "L5";
+            this.L5.Size = new System.Drawing.Size(140, 20);
+            this.L5.TabIndex = 3;
+            this.L5.Visible = false;
+            // 
+            // L6
+            // 
+            this.L6.BackColor = System.Drawing.Color.LightGreen;
+            this.L6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.L6.Location = new System.Drawing.Point(95, 344);
+            this.L6.Name = "L6";
+            this.L6.Size = new System.Drawing.Size(130, 20);
+            this.L6.TabIndex = 3;
+            this.L6.Visible = false;
+            // 
+            // L7
+            // 
+            this.L7.BackColor = System.Drawing.Color.LightGreen;
+            this.L7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.L7.Location = new System.Drawing.Point(100, 324);
+            this.L7.Name = "L7";
+            this.L7.Size = new System.Drawing.Size(120, 20);
+            this.L7.TabIndex = 3;
+            this.L7.Visible = false;
+            // 
+            // L8
+            // 
+            this.L8.BackColor = System.Drawing.Color.LightGreen;
+            this.L8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.L8.Location = new System.Drawing.Point(105, 304);
+            this.L8.Name = "L8";
+            this.L8.Size = new System.Drawing.Size(110, 20);
+            this.L8.TabIndex = 3;
+            this.L8.Visible = false;
+            // 
+            // L9
+            // 
+            this.L9.BackColor = System.Drawing.Color.LightGreen;
+            this.L9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.L9.Location = new System.Drawing.Point(110, 284);
+            this.L9.Name = "L9";
+            this.L9.Size = new System.Drawing.Size(100, 20);
+            this.L9.TabIndex = 3;
+            this.L9.Visible = false;
+            // 
+            // L10
+            // 
+            this.L10.BackColor = System.Drawing.Color.LightGreen;
+            this.L10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.L10.Location = new System.Drawing.Point(115, 264);
+            this.L10.Name = "L10";
+            this.L10.Size = new System.Drawing.Size(90, 20);
+            this.L10.TabIndex = 3;
+            this.L10.Visible = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 45);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(94, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Enabled = false;
+            this.btnStart.Location = new System.Drawing.Point(22, 16);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 502);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.L10);
+            this.Controls.Add(this.L9);
+            this.Controls.Add(this.L8);
+            this.Controls.Add(this.L7);
+            this.Controls.Add(this.L6);
+            this.Controls.Add(this.L5);
+            this.Controls.Add(this.L4);
+            this.Controls.Add(this.L3);
+            this.Controls.Add(this.L2);
+            this.Controls.Add(this.L1);
+            this.Controls.Add(this.LA);
+            this.Controls.Add(this.LC);
+            this.Controls.Add(this.LB);
+            this.Controls.Add(this.LblBase);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hanoy";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblBase;
+        private System.Windows.Forms.Label LB;
+        private System.Windows.Forms.Label LA;
+        private System.Windows.Forms.Label LC;
+        private System.Windows.Forms.Label L1;
+        private System.Windows.Forms.Label L2;
+        private System.Windows.Forms.Label L3;
+        private System.Windows.Forms.Label L4;
+        private System.Windows.Forms.Label L5;
+        private System.Windows.Forms.Label L6;
+        private System.Windows.Forms.Label L7;
+        private System.Windows.Forms.Label L8;
+        private System.Windows.Forms.Label L9;
+        private System.Windows.Forms.Label L10;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ HanoyPrj/Form1.cs
@@ -0,0 +1,439 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HanoyPrj
+{
+    public partial class Form1 : Form
+    {
+        Control topControlA;
+        Control topControlB;
+        Control topControlC;
+
+        int topA;
+        int topB;
+        int topC;
+        Stack<Control> myStackA = new Stack<Control>();
+        Stack<Control> myStackB = new Stack<Control>();
+        Stack<Control> myStackC = new Stack<Control>();
+
+        int src, des;
+        int totalDisk;
+        string direction = "up";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {            
+            for (int i = 1; i <= 10; i++)
+            {
+                comboBox1.Items.Add(i);
+            }          
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int a;
+            clearDisk();
+            totalDisk = int.Parse(comboBox1.Text);
+            foreach (Control c in Controls)
+            {
+                if (c is Label && int.TryParse(c.Name.Substring(1), out a))
+                {
+                    if (int.Parse(c.Name.Substring(1)) >= 1 && int.Parse(c.Name.Substring(1)) <= totalDisk)
+                    {
+                        c.Visible = true;
+                    }
+                }
+            }
+            for (int i = 1; i <=totalDisk; i++)
+            {
+                foreach (Control c in Controls)
+                {
+                    if (c is Label && c.Name == "L" + i)
+                    {
+                        myStackA.Push(c);
+                        break;
+                    }
+                }
+            }          
+
+            foreach (Control c in Controls)
+            {
+                if (c.Name == "L" + totalDisk)
+                {
+                    topControlA = c;
+                    topControlB = null;
+                    topControlC = null;
+                    break;
+                }
+
+            }
+            topA = 464 - totalDisk * 20;
+            topB = 464;
+            topC = 464;
+            btnStart.Enabled = true;
+        }
+
+        private void clearDisk()
+        {
+            L1.Visible = false;
+            L2.Visible = false;
+            L3.Visible = false;
+            L4.Visible = false;
+            L5.Visible = false;
+            L6.Visible = false;
+            L7.Visible = false;
+            L8.Visible = false;
+            L9.Visible = false;
+            L10.Visible = false;
+
+            L1.Left = 70;
+            L2.Left = 75;
+            L3.Left = 80;
+            L4.Left = 85;
+            L5.Left = 90;
+            L6.Left = 95;
+            L7.Left = 100;
+            L8.Left = 105;
+            L9.Left = 110;
+            L10.Left = 115;
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            int n = int.Parse(comboBox1.Text);
+            hanoyTower(n,1,2,3);
+        }
+
+        private void hanoyTower(int n, int v1, int v2, int v3)
+        {
+            if (n == 1)
+                moveDisk(v1, v3);
+            else
+            {
+                hanoyTower(n - 1, v1, v3, v2);
+                moveDisk(v1,v3);
+                hanoyTower(n - 1, v2, v1, v3);
+            }
+        }
+
+        
+        private void Transfer(int src,int des)
+        {
+            while (true)
+            {
+                if (src == 1 && des == 2)
+                {
+                    if (direction == "up")
+                    {
+                        topControlA.Top -= 15;
+                        this.Refresh();
+                        System.Threading.Thread.Sleep(20);
+                        if (topControlA.Top < 80)
+                        {
+                            direction = "right";
+                        }
+                    }
+                    else if (direction == "right")
+                    {
+                        topControlA.Left += 15;
+                        this.Refresh();
+                        System.Threading.Thread.Sleep(20);
+                        if (topControlA.Left > LB.Left + 10 - (topControlA.Width / 2))
+                        {
+                            topControlA.Left = LB.Left + 10 - (topControlA.Width / 2);
+                            direction = "down";
+                        }
+                    }
+                    else if (direction == "down")
+                    {
+                        topControlA.Top += 15;
+                        this.Refresh();
+                        System.Threading.Thread.Sleep(20);
+                        if (topControlA.Top > topB - 20)
+                        {
+                            topControlA.Top = topB - 20;
+                            topControlB = topControlA;
+                            myStackB.Push(myStackA.Pop());
+                            if (myStackA.Count == 0)
+                                topControlA = null;
+                            else
+                                topControlA = myStackA.Peek();
+                            direction = "up";
+                            topA += 20;
+                            topB -= 20;                            
+                            break;
+                        }
+                    }
+                }
+                else if (src == 1 && des == 3)
+                {
+                    if (direction == "up")
+                    {
+                        topControlA.Top -= 15;
+                        this.Refresh();
+                        System.Threading.Thread.Sleep(20);
+                        if (topControlA.Top < 80)
+                        {
+                            direction = "right";
+                        }
+                    }
+                    else if (direction == "right")
+                    {
+                        topControlA.Left += 15;
+                        this.Refresh();
+                        System.Threading.Thread.Sleep(20);
+                        if (topControlA.Left > LC.Left + 10 - (topControlA.Width / 2))
+                        {
+                            topControlA.Left = LC.Left + 10 - (topControlA.Width / 2);
+                            direction = "down";
+                        }
+                    }
+                    else if (direction == "down")
+                    {
+                        topControlA.Top += 15;
+                        this.Refresh();
+                        System.Threading.Thread.Sleep(20);
+                        if (topControlA.Top > topC - 20)
+                        {
+                            topControlA.Top = topC - 20;
+                            topControlC = topControlA;
+                            myStackC.Push(myStackA.Pop());
+                            if (myStackA.Count == 0)
+                                topControlA = null;
+                            else
+                                topControlA = myStackA.Peek();
+                            direction = "up";
+                            topA += 20;
+                            topC -= 20;                            
+                            break;
+                        }
+                    }
+                }
+                else if (src == 2 && des == 1)
+                {
+                    if (direction == "up")
+                    {
+                        topControlB.Top -= 15;
+                        this.Refresh();
+                        System.Threading.Thread.Sleep(20);
+                        if (topControlB.Top < 80)
+                        {
+                            direction = "left";
+                        }
+                    }
+                    else if (direction == "left")
+                    {
+                        topControlB.Left -= 15;
+                        this.Refresh();
+                        System.Threading.Thread.Sleep(20);
+                        if (topControlB.Left < LA.Left + 10 - (topControlB.Width / 2))
+                        {
+                            topControlB.Left = LA.Left + 10 - (topControlB.Width / 2);
+                            direction = "down";
+                        }
+                    }
+                    else if (direction == "down")
+                    {
+                        topControlB.Top += 15;
+                        this.Refresh();
+                        System.Threading.Thread.Sleep(20);
+                        if (topControlB.Top > topA - 20)
+                        {
+                            topControlB.Top = topA - 20;
+                            topControlA = topControlB;
+                            myStackA.Push(myStackB.Pop());
+                            if (myStackB.Count == 0)
+                                topControlB = null;
+                            else
+                                topControlB = myStackB.Peek();
+                            direction = "up";
+                            topB += 20;
+                            topA -= 20;                            
+                            break;
+                        }
+                    }
+                }
+                else if (src == 2 && des == 3)
+                {
+                    if (direction == "up")
+                    {
+                        topControlB.Top -= 15;
+                        this.Refresh();
+                        System.Threading.Thread.Sleep(20);
+                        if (topControlB.Top < 80)
+                        {
+                            direction = "right";
+                        }
+                    }
+                    else if (direction == "right")
+                    {
+                        topControlB.Left += 15;
+                        this.Refresh();
+                        System.Threading.Thread.Sleep(20);
+                        if (topControlB.Left > LC.Left + 10 - (topControlB.Width / 2))
+                        {
+                            topControlB.Left = LC.Left + 10 - (topControlB.Width / 2);
+                            direction = "down";
+                        }
+                    }
+                    else if (direction == "down")
+                    {
+                        topControlB.Top += 15;
+                        this.Refresh();
+                        System.Threading.Thread.Sleep(20);
+                        if (topControlB.Top > topC - 20)
+                        {
+                            topControlB.Top = topC - 20;
+                            topControlC = topControlB;
+                            myStackC.Push(myStackB.Pop());
+                            if (myStackB.Count == 0)
+                                topControlB = null;
+                            else
+                                topControlB = myStackB.Peek();
+                            direction = "up";
+                            topB += 20;
+                            topC -= 20;                            
+                            break;
+                        }
+                    }
+                }
+                else if (src == 3 && des == 1)
+                {
+                    if (direction == "up")
+                    {
+                        topControlC.Top -= 15;
+                        this.Refresh();
+                        System.Threading.Thread.Sleep(20);
+                        if (topControlC.Top < 80)
+                        {
+                            direction = "left";
+                        }
+                    }
+                    else if (direction == "left")
+                    {
+                        topControlC.Left -= 15;
+                        this.Refresh();
+                        System.Threading.Thread.Sleep(20);
+                        if (topControlC.Left < LA.Left + 10 - (topControlC.Width / 2))
+                        {
+                            topControlC.Left = LA.Left + 10 - (topControlC.Width / 2);
+                            direction = "down";
+                        }
+                    }
+                    else if (direction == "down")
+                    {
+                        topControlC.Top += 15;
+                        this.Refresh();
+                        System.Threading.Thread.Sleep(20);
+                        if (topControlC.Top > topA - 20)
+                        {
+                            topControlC.Top = topA - 20;
+                            topControlA = topControlC;
+                            myStackA.Push(myStackC.Pop());
+                            if (myStackC.Count == 0)
+                                topControlC = null;
+                            else
+                                topControlC = myStackC.Peek();
+                            direction = "up";
+                            topA -= 20;
+                            topC += 20;                           
+                            break;
+                        }
+                    }
+                }
+                else if (src == 3 && des == 2)
+                {
+                    if (direction == "up")
+                    {
+                        topControlC.Top -= 15;
+                        this.Refresh();
+                        System.Threading.Thread.Sleep(20);
+                        if (topControlC.Top < 80)
+                        {
+                            direction = "left";
+                        }
+                    }
+                    else if (direction == "left")
+                    {
+                        topControlC.Left -= 15;
+                        this.Refresh();
+                        System.Threading.Thread.Sleep(20);
+                        if (topControlC.Left < LB.Left + 10 - (topControlC.Width / 2))
+                        {
+                            topControlC.Left = LB.Left + 10 - (topControlC.Width / 2);
+                            direction = "down";
+                        }
+                    }
+                    else if (direction == "down")
+                    {
+                        topControlC.Top += 15;
+                        this.Refresh();
+                        System.Threading.Thread.Sleep(20);
+                        if (topControlC.Top > topB - 20)
+                        {
+                            topControlC.Top = topB - 20;
+                            topControlB = topControlC;
+                            myStackB.Push(myStackC.Pop());
+                            if (myStackC.Count == 0)
+                                topControlC = null;
+                            else
+                                topControlC = myStackC.Peek();
+                            direction = "up";
+                            topC += 20;
+                            topB -= 20;                            
+                            break;
+                        }
+                    }
+                }
+            }            
+        }
+        
+
+        private void moveDisk(int v1, int v3)
+        {
+            
+            if(v1==1 && v3==2)
+            {
+                src = 1;
+                des = 2;
+            }
+            else if(v1==1 && v3 == 3)
+            {
+                src = 1;
+                des = 3;
+            }
+            else if (v1 == 2 && v3 == 1)
+            {
+                src = 2;
+                des = 1;
+            }
+            else if (v1 == 2 && v3 == 3)
+            {
+                src = 2;
+                des = 3;
+            }
+            else if (v1 == 3 && v3 == 1)
+            {
+                src = 3;
+                des = 1;
+            }
+            else if (v1 == 3 && v3 == 2)
+            {
+                src = 3;
+                des = 2;
+            }
+            Transfer(src, des);
+        }
+    }
+}
